1a6f86f2a2cb3820505ab8d8d856da0343488c08
Kenny Williams <kennyjwilli@gmail.com>
2016-04-05 14:59:31 -0700

    Run boot lein-generate

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject boot-gorilla "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [gorilla-repl "0.3.5"]
-                 [boot/core "2.5.1"]])
+(defproject
+  boot-gorilla
+  "0.1.0"
+  :dependencies
+  [[org.clojure/clojure "1.8.0"] [gorilla-repl "0.3.6"]]
+  :source-paths
+  ["src" "test" "bower_components" "src" "scss" "test"])
